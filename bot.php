ed150b89588d27860a657cfac33bea0aa19158fa
Prawit Boonthue <prawit.boonthue@gmail.com>
2017-01-04 20:41:12 +0700

    Hotfix: edit headers

--- bot.php
+++ bot.php
@@ -35,7 +35,7 @@ if (!is_null($events['events'])) {
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
       curl_close($ch);
