d7038481507fdcd5c9395bbeea54bdb99a91ee6a
Prawit Boonthue <prawit.boonthue@gmail.com>
2017-01-05 11:26:58 +0700

    Add proxy

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'http://fixie:AHw2NimN7t5EsSh@velodrome.usefixie.com:80';
+$proxyauth = 'prawit.boonthue@gmail.com:tstcfsteA1';
+
 $access_token = 'AToSRdI67TV4soIwl2L1HlT7jGBLx4vIf1RmocOt8rJG9/n7afVpD8psgm6VUNbfEvR+LYQOMQ88xa3YTeh00zGOU68TJ5PlZ7koYVIn3cuI94605PLoQu5RFT59FY1za6iOzL9wR3Iy+HjejULbrAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
