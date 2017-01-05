22a0ce9c2b6b2651da0eb09d5fd3edcf375c62fd
Prawit Boonthue <prawit.boonthue@gmail.com>
2017-01-05 12:28:48 +0700

    Hotfix weather str_replace line

--- bot.php
+++ bot.php
@@ -22,14 +22,13 @@ if (!is_null($events['events'])) {
       // Split a string by colon
       $sentences = explode(":", $text);
       // Forecast the weather
-      if ($sentences[0] === "อากาศ") {
-        $url_weather = "http://api.wunderground.com/api/152abfcd8a423756/forecast/q/Thailand/" . str_replace(' ', '%20', $sentences[1]).'.json');
-        // $json_weather = file_get_contents($url_weather);
-        // $parsed_weather = json_decode($json_weather, true);
-        // $location = $parsed_json->{'location'}->{'city'};
-        // $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
-        // $text = "Current temperature in ${location} is: ${temp_c}\n";
-        $text = sentences[1];
+      if ($sentences[0] == "อากาศ") {
+        $url_weather = "http://api.wunderground.com/api/152abfcd8a423756/forecast/q/Thailand/" . str_replace(' ', '%20', $sentences[1]) . '.json';
+        $json_weather = file_get_contents($url_weather);
+        $parsed_weather = json_decode($json_weather, true);
+        $location = $parsed_json->{'location'}->{'city'};
+        $temp_c = $parsed_json->{'current_observation'}->{'temp_c'};
+        $text = "Current temperature in ${location} is: ${temp_c}\n";
       }
 
       // Build message to reply back
